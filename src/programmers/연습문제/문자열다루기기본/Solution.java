997ece208080955032d94158152ced2d2d72fd49
jaechanyoon <wocksl1998@gmail.com>
2022-08-26 22:24:13 +0900

    programmers

--- src/programmers/연습문제/문자열다루기기본/Solution.java
+++ src/programmers/연습문제/문자열다루기기본/Solution.java
@@ -2,11 +2,11 @@ package src.programmers.연습문제.문자열다루기기본;
 
 class Solution {
     public boolean solution(String s) {
-        if(s.length() != 4 && s.length() != 6){
+        if (s.length() != 4 && s.length() != 6) {
             return false;
         }
-        for(int i = 0; i < s.length(); i++){
-            if('0' > s.charAt(i) || s.charAt(i) > '9'){
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) < '0' || '9' < s.charAt(i)) {
                 return false;
             }
         }
